fa0b2cd415f32303d89601c6cf5202e4d0604c22
Aad Versteden <madnificent@gmail.com>
2024-10-01 14:52:08 +0200

    Manhandling into pytorch template
    The existing code now more-or-less fits in the Pytorch template.  yay!
    
    Automated builds have to be set up still.

---
+++ embedding_service/sparql.py
@@ -0,0 +1,34 @@
+from escape_helpers import sparql_escape_string, sparql_escape_uri
+from helpers import query, update
+from string import Template
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+# TODO: load from file
+QUERY_GET_ALL_PRODUCTS = """
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX ipdc: <https://productencatalogus.data.vlaanderen.be/ns/ipdc-lpdc#>
+
+SELECT ?subject ?title ?description
+WHERE {
+  GRAPH <http://mu.semte.ch/graphs/public> {
+    ?subject ?p ipdc:InstancePublicServiceSnapshot .
+    ?subject dcterms:title ?title .
+    FILTER (lang(?title) = "nl")
+    ?subject dcterms:description ?description .
+    FILTER (lang(?description) = "nl")
+  }
+}
+ORDER BY ?subject
+"""
+
+def get_all_products():
+    results_raw = query(QUERY_GET_ALL_PRODUCTS)
+
+    print("We have {} products".format( len(results_raw['results']['bindings']) ))
+
+    products = {}
+    for prod in results_raw['results']['bindings']:
+        products[prod['subject']['value']] = prod['title']['value'] + " | " + prod["description"]['value']
+
+    return products
--- web.py
+++ web.py
@@ -1,11 +1,68 @@
 from embedding_service.embed import get_embedding
 from embedding_service.sparql import get_all_products
+import json
+from threading import Thread
+from requests import post
+from escape_helpers import sparql_escape_string, sparql_escape_uri
+from helpers import query, update
+import time
 
-products = get_all_products()
+def getEmbeddings():
+    queryResults = query("""PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+SELECT ?sub ?embedding
+WHERE {
+    VALUES (?embedding) { ("[1,3,3,7]") }
+    ?sub a <https://productencatalogus.data.vlaanderen.be/ns/ipdc-lpdc#InstancePublicServiceSnapshot>.
+}""")
+    # ?sub ext:hasEmbedding ?embedding.
 
-for id, prod_descript in products.items():
-    emb = get_embedding(prod_descript)
+    print("got results")
 
+    # TODO: loop and combine paginated responses
+
+    return [(binding["sub"]["value"], json.loads(binding["embedding"]["value"]))
+            for binding in queryResults["results"]["bindings"]]
+
+@app.route("/query-weights")
+def queryWeights():
+    queryEmbeddings = json.loads(request.args.get('query'))
+    dataEmbeddings = getEmbeddings()
+
+    # TODO: find optimal solution
+
+    # TODO: return ?sub (first half of tuple in embeddings)
+
+# TODO: future, maybe in hackathon
+@app.route("/query-sentence")
+def querySentence():
+    # TODO: we _might_ want to implement this.  This will wire services
+    # together but this part of AI is more systems programming so that
+    # makes sense.
+    print("hello")
+
+
+###################
+### FUTURE WORK ###
+###################
+
+# FUTURE: ingest embeddings at boot
+
+# embeddings = [
+#   # ("http://something/23423", [34,.433,4.43,.34,.34,34.,34534,.3,]), ...
+# ]
+#
+
+@app.route("/ingest", methods=["POST"])
+def ingest():
     # TODO inject in DB
+    products = get_all_products()
 
+    for id, prod_descript in products.items():
+        emb = get_embedding(prod_descript)
 
+def ingestWithDelay():
+    time.sleep(1)
+    post("http://localhost/ingest")
+
+Thread(target = lambda: ingestWithDelay()).start()
